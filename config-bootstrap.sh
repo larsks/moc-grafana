50e421d69eb31141b9da751139db29cc2d3d9a80
Lars Kellogg-Stedman <lars@redhat.com>
2019-05-03 19:58:12 -0400

    move all config bootstrap into shell script

--- config-bootstrap.sh
+++ config-bootstrap.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 yum -y upgrade
 yum -y install yum-utils epel-release git
 yum-config-manager --disable epel
@@ -10,5 +12,28 @@ if ! needs-restarting -r; then
   reboot
 fi
 
-exit 0
+cat > /etc/systemd/system/ansible-pull@.service <<'EOF'
+[Service]
+Type=oneshot
+EnvironmentFile=/etc/ansible/pull/%i/config
+ExecStart=/usr/bin/ansible-pull -U ${PULL_REPOSITORY_URL}
+EOF
 
+cat > /etc/systemd/system/ansible-pull@.timer <<'EOF'
+[Timer]
+OnCalendar=hourly
+RandomizedDelaySec=600
+Persistent=true
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+mkdir -p /etc/ansible/pull/moc_grafana
+cat > /etc/ansible/pull/moc_grafana/config <<'EOF'
+PULL_REPOSITORY_URL=__ansible_repository_url__
+EOF
+
+systemctl daemon-reload
+systemctl enable --now ansible-pull@moc_grafana.timer
+systemctl start ansible-pull@moc_grafana.service
